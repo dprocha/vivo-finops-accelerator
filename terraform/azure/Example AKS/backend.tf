f20b77c17e864c6e0f9c80daa70692540fe66a1c
Diego Pereira da Rocha <dprocha85@gmail.com>
2022-12-21 17:33:00 -0300

    First Commit

--- terraform/azure/Example AKS/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-aleteia-test"
-    storage_account_name = "sttfstatealeteiatest"
-    container_name       = "remote-state"
-    key                  = "azure-hml/terraform.tfstate"
-  }
-}
